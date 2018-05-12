cf70231e2fb8867de8e0b99b8b7c04478994b014
stromy <stromy@protonmail.com>
2018-05-12 18:26:07 +0200

    Added recording

--- app/src/main/java/com/example/tunergitarowy/MainActivity.java
+++ app/src/main/java/com/example/tunergitarowy/MainActivity.java
@@ -1,14 +1,78 @@
 package com.example.tunergitarowy;
 
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    // Test asdf
-    // test kris
+
+    private RecordingThread recordingThread;
+    private Button recordButton;
+    private TextView helloView;
+    private static final int REQUEST_RECORD_AUDIO = 13;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        helloView = this.findViewById(R.id.textView);
+
+        recordButton = this.findViewById(R.id.record);
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!recordingThread.recording()) {
+                    startAudioRecordingSafe();
+                    helloView.setText("Started recording!");
+                }else{
+                    recordingThread.stopRecording();
+                    helloView.setText("Stopped recording!");
+                }
+            }
+        });
+
+        recordingThread = new RecordingThread(new AudioDataReceivedListener() {
+            @Override
+            public void onAudioDataReceived(short[] data) {
+
+            }
+        });
+
+
+    }
+
+    private void startAudioRecordingSafe() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO)
+                == PackageManager.PERMISSION_GRANTED) {
+            recordingThread.startRecording();
+        } else {
+            requestMicrophonePermission();
+        }
+    }
+
+    private void requestMicrophonePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.RECORD_AUDIO)) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    android.Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_AUDIO);
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    android.Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_AUDIO);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,  String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_RECORD_AUDIO && grantResults.length > 0 &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            recordingThread.stopRecording();
+        }
     }
 }
