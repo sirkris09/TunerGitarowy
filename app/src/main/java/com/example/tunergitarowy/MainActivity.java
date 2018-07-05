d7253b8e12290336da419116659a46503e4b1155
stromy <stromy@protonmail.com>
2018-07-05 22:28:12 +0200

    Added TunerActivity

--- app/src/main/java/com/example/tunergitarowy/MainActivity.java
+++ app/src/main/java/com/example/tunergitarowy/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tunergitarowy;
 
 import android.Manifest;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -12,70 +13,29 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecordingThread recordingThread;
-    private TunerView tunerView;
     private Button recordButton;
     private TextView helloView;
     private static final int REQUEST_RECORD_AUDIO = 13;
 
+    public static final String EXTRA_INT = "com.example.tunergitarowy.INT";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        tunerView = (TunerView) findViewById(R.id.tunerview);
-
-
-        helloView = this.findViewById(R.id.textView);
 
         recordButton = this.findViewById(R.id.record);
         recordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!recordingThread.recording()) {
-                    startAudioRecordingSafe();
-                    helloView.setText("Started recording!");
-                }else{
-                    recordingThread.stopRecording();
-                    helloView.setText("Stopped recording!");
-                }
+                startTuner(0);
             }
         });
-
-        recordingThread = new RecordingThread(new AudioDataReceivedListener() {
-            @Override
-            public void onAudioDataReceived(short[] data) {
-                tunerView.transferSamples(data);
-            }
-        });
-
-
     }
 
-    private void startAudioRecordingSafe() {
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO)
-                == PackageManager.PERMISSION_GRANTED) {
-            recordingThread.startRecording();
-        } else {
-            requestMicrophonePermission();
-        }
-    }
-
-    private void requestMicrophonePermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.RECORD_AUDIO)) {
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
-                    android.Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_AUDIO);
-        } else {
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
-                    android.Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_AUDIO);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,  String[] permissions, int[] grantResults) {
-        if (requestCode == REQUEST_RECORD_AUDIO && grantResults.length > 0 &&
-                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            recordingThread.stopRecording();
-        }
+    private void startTuner(int range_selector){
+        Intent intent = new Intent(this, TunerActivity.class);
+        intent.putExtra(this.EXTRA_INT, range_selector);
+        startActivity(intent);
     }
 }
